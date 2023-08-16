2b2203101fae040be90a4c71765d9c062bc55d64
Sma1lboy <541898146chen@gmail.com>
2023-08-16 13:37:29 +0800

    encaplication api and axios

---
+++ src/pages/home-page/tabs/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+
+export default function index() {
+  return (
+    <Box sx={{window: "100%"}}>
+      <Box sx={{borderBottom: 1, borderColor: "divider"}}></Box>
+    </Box>
+  );
+}
