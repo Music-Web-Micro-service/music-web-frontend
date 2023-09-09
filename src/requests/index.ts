3e64a5af727b02f36a87225ca588ba600cd563ac
Sma1lboy <541898146chen@gmail.com>
2023-09-09 09:57:55 -0500

    api封装

--- src/requests/index.ts
+++ src/requests/index.ts
@@ -1,14 +1,9 @@
 import axios from "axios";
-import QS from "qs";
-
-const isProduction = process.env.NODE_ENV == "production";
-
-export const basicUrl = isProduction
-  ? "https://localhost:3370"
-  : "http://www.CHANGE-LATER.com";
+import {BASE_URL, TIMEOUT} from "./config";
 
 const service = axios.create({
-  baseURL: basicUrl,
+  baseURL: BASE_URL,
+  timeout: TIMEOUT,
 });
 
 //TODO 后续需要在这里写一个filter or intercepter 用来判断local storage或者session sotrage有无token，进行拦截操作
---
+++ src/requests/request.ts
@@ -0,0 +1,10 @@
+import {AxiosRequestConfig} from "axios";
+import serivce from "./index";
+
+export const apiTestApiAdd = (info: AxiosRequestConfig) => serivce.post("/api/test/api", info);
+//call example
+
+// apiArticleEdit(params).then(res => {
+//     console.log(res)
+//   })
+// }
