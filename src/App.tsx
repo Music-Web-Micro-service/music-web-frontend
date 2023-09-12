c1e9b6426017195a2e27cefee2b74d4c037fa96e
ZHallen122 <zhallen12261@gmail.com>
2023-09-11 21:58:52 -0400

    add TrackProvider the global hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,53 @@
-import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./Nav";
 import Home from "./pages/Home";
 import Favorite from "./pages/Favorite";
 import DownloadHistory from "./pages/DownloadHistory";
 import NewPlaylist from "./pages/NewPlaylist";
 import "./App.css";
-import {ThemeProvider} from "@emotion/react";
+import { ThemeProvider } from "@emotion/react";
 import theme from "./themes/theme";
 import HomeTabs from "./pages/home-page/tabs";
-import {Box} from "@mui/material";
+import AlbumPage from "./pages/AlbumPage";
+import { Box } from "@mui/material";
+import PlayMusicBar from "./components/PlayMusicBar";
+import { useTrack } from "./hook/TrackHook";
+import { TrackProvider } from "./hook/TrackContext";
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Box className="app-container" display={"flex"} flexDirection={"row"} width={1}>
-          <Nav />
-          <Box display={"flex"} flexDirection={"column"} width={1}>
-            <HomeTabs></HomeTabs>
-            <Box className="main-content">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/favorite" element={<Favorite />} />
-                <Route path="/download-history" element={<DownloadHistory />} />
-                <Route path="/new-playlist" element={<NewPlaylist />} />
-              </Routes>
+    <TrackProvider>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Box className="app-container" display={"flex"} flexDirection={"row"} width={1}>
+            <Nav />
+            <Box display={"flex"} flexDirection={"column"} width={1}>
+              <HomeTabs></HomeTabs>
+              <Box className="main-content">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/favorite" element={<Favorite />} />
+                  <Route path="/download-history" element={<DownloadHistory />} />
+                  <Route path="/new-playlist" element={<NewPlaylist />} />
+                </Routes>
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </Router>
-    </ThemeProvider>
+        </Router>
+        <MusicBarComponent />
+      </ThemeProvider>
+    </TrackProvider>
+  );
+};
+
+const MusicBarComponent = () => {
+  const { isPlaying } = useTrack();
+  const [hasPlayed] = useState(true);
+  return (
+    <>
+      {isPlaying ? <PlayMusicBar /> : hasPlayed && <PlayMusicBar />}
+    </>
   );
 };
 
