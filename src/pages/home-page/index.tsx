7021b52456a8cbb1554a4c5da00ebb0f364dc986
Sma1lboy <541898146chen@gmail.com>
2023-11-29 11:32:14 -0600

    delete testing muisc track url

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -3,13 +3,12 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Nav from "../../Nav";
 import PlayMusicBar from "../../components/PlayMusicBar";
-import { TrackProvider } from "../../hook/TrackContext";
 import { useTrack } from "../../hook/TrackHook";
 import HomeTabs from "./tabs";
 
 
 export default function HomePage() {
-  return (<TrackProvider>
+  return (
     <Box className="app-container" display={"flex"} flexDirection={"row"} width={1}>
       <Nav />
       <Box display={"flex"} flexDirection={"column"} sx={{width: window.innerWidth - 250}}>
@@ -20,10 +19,27 @@ export default function HomePage() {
         </Box>
       </Box>
     </Box>
-    <MusicBarComponent />
-  </TrackProvider>
+
   );
 }
+
+//TODO fix this
+// export default function HomePage() {
+//   return (<TrackProvider>
+//     <Box className="app-container" display={"flex"} flexDirection={"row"} width={1}>
+//       <Nav />
+//       <Box display={"flex"} flexDirection={"column"} sx={{width: window.innerWidth - 250}}>
+//         <HomeTabs></HomeTabs>
+//         <Box className="main-content">
+//           {/* Outlet makes render route */}
+//           <Outlet />
+//         </Box>
+//       </Box>
+//     </Box>
+//     <MusicBarComponent />
+//   </TrackProvider>
+//   );
+// }
 
 const MusicBarComponent = () => {
   const {isPlaying} = useTrack();
