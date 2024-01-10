1b6ed64dd1eac26a3227c1cf9e95e60cb645c74a
crystal <crystalhqx@hotmail.com>
2024-01-09 23:14:28 -0500

    updated creating playlist

--- src/pages/home-page/NewPlaylistDialog.tsx
+++ src/pages/home-page/NewPlaylistDialog.tsx
@@ -1,32 +1,33 @@
-import {
-  Button,
-  Checkbox,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  FormControlLabel,
-  TextField,
-} from "@mui/material";
-import React, { useState } from "react";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField} from "@mui/material";
+import {createPlaylist} from "../../apis/PlaylistApis";
 
 interface NewPlaylistDialogProps {
   open: boolean;
   onClose: () => void;
-  onCreate: (name: string, description: string, isPublic: boolean) => void;
+  onCreate: (name: string, description: string) => Promise<void>;
 }
 
 const NewPlaylistDialog: React.FC<NewPlaylistDialogProps> = ({open, onClose, onCreate}) => {
   const [playlistName, setPlaylistName] = useState("");
   const [playlistDescription, setPlaylistDescription] = useState("");
-  const [isPublic, setIsPublic] = useState(true);
+  const navigate = useNavigate();
 
-  const handleCreate = () => {
-    onCreate(playlistName, playlistDescription, isPublic);
-    setPlaylistName("");
-    setPlaylistDescription("");
-    setIsPublic(true);
-    onClose();
+  const getUserId = () => {
+    // TODO: This is only a fake data, should be updated after adding login logic
+    return 1;
+  };
+
+  const handleCreate = async () => {
+    try {
+      await onCreate(playlistName, playlistDescription);
+      setPlaylistName("");
+      setPlaylistDescription("");
+      onClose();
+    } catch (error) {
+      console.error("Error creating playlist:", error);
+    }
   };
 
   return (
@@ -53,17 +54,6 @@ const NewPlaylistDialog: React.FC<NewPlaylistDialogProps> = ({open, onClose, onC
           variant="standard"
           value={playlistDescription}
           onChange={(e) => setPlaylistDescription(e.target.value)}
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={isPublic}
-              onChange={(e) => setIsPublic(e.target.checked)}
-              name="public"
-              color="primary"
-            />
-          }
-          label="Public"
         />
       </DialogContent>
       <DialogActions>
